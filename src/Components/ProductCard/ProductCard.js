6c7891874e878f21902f9b345a3e102e38138dda
Sandeep Bisht <>
2023-07-01 15:14:17 +0530

    handle wishlist in homepage

--- src/Components/ProductCard/ProductCard.js
+++ src/Components/ProductCard/ProductCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import "./ProductCard.css";
 import Cookies from "js-cookie";
 import product1 from "../../Images/product/product1.jpg";
@@ -8,27 +8,88 @@ import { AiOutlineSearch, AiOutlineShoppingCart } from "react-icons/ai";
 import { BsBagHeart } from "react-icons/bs";
 import { BsEye } from "react-icons/bs";
 import { Link, useNavigate } from "react-router-dom";
-import { async } from "q";
+import axios from "axios";
+import { useToasts } from "react-toast-notifications";
 
 const ProductCard = (props) => {
+
+  let { addToast } = useToasts();
+  const [wishlistItem, setWishlistItem] = useState([]);
+  const [userdata, setUserdata] = useState();
   const { productList } = props;
   let url = "http://localhost:8080/";
   let navigate = useNavigate();
 
   console.log(productList, "product list in product car");
 
-  // Re Direction to single product page  
+  useEffect(() =>{
+    let userdata = JSON.parse(decodeURIComponent(Cookies.get("userdata")))
+    setUserdata(userdata);
+    getUserWishlist(userdata._id)
+  },[])
+
+  console.log(wishlistItem, "wishlistItem wishlistItem")
+
+  // Re Direction to single product page
   let redirectToProductDiscriptionPage = (name) => {
     // navigate(`/product/${name}`, { state: productId });
     navigate(`/product/${name}`);
   };
 
   // Add to wishlist
-  const onClickWishListHandler = async(productId) => {
-    const cookieValue = Cookies.get("userdata");
-const userdata = JSON.parse(decodeURIComponent(cookieValue));
-console.log(userdata, "userdata")
-    console.log(productId, "productId productId")
+  const onClickWishListHandler = async (productId) => { 
+    let data = {}; 
+    const foundNumber = wishlistItem.find((item) => item.productId._id === productId);
+    console.log("foundNumber", foundNumber)
+      if(foundNumber){
+
+        addToast("Success!", {
+          appearance: "success",
+          content: `Product is already in wishlist`,
+        });
+        
+      } else{
+        let userId = userdata._id;
+        data["productId"] =productId;
+        data["userId"] = userId;
+    
+        
+        let url = "http://localhost:8080/api/wishlist/add_to_wishlist"
+        let response = await axios.post(url, data);
+        try {
+          if(response){
+            getUserWishlist(userdata._id)
+            console.log(response)
+            addToast("Success!", {
+              appearance: "success",
+              content: `Product added to wishlist`,
+            });
+          }
+        } catch (error) {
+          console.log(error)
+          addToast("error!", {
+            appearance: "error",
+            content: `Something went wrong`,
+          });
+        }
+        
+      }
+ 
+  };
+
+  // Get Wishlist Item
+  const getUserWishlist = async (userid) => {
+    let url = "http://localhost:8080/api/wishlist/wishlist_by_id";
+    let response = await axios.post(url, userid);
+      try {
+        if(response){
+           console.log(response,"response of produsct") 
+           setWishlistItem(response.data.data) 
+         
+        }
+      } catch (error) {
+        console.log(error)
+      }
   }
 
   return (
@@ -61,21 +122,17 @@ console.log(userdata, "userdata")
                         <div className="product-content-lower">
                           <ul>
                             <li>
-                              
-                                <span className="product-card-icon">
-                                  <AiOutlineShoppingCart />
-                                </span>
-                             
+                              <span className="product-card-icon">
+                                <AiOutlineShoppingCart />
+                              </span>
                             </li>
 
-                            <li onClick={()=>onClickWishListHandler(item._id)}>
-                             
-                                <span className="product-card-icon"
-                                
-                                >
-                                  <BsBagHeart />
-                                </span>
-                              
+                            <li
+                              onClick={() => onClickWishListHandler(item._id)}
+                            >
+                              <span className="product-card-icon">
+                                <BsBagHeart />
+                              </span>
                             </li>
                           </ul>
                         </div>
