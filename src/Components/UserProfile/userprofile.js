9e5e0af43efb3124d510b7292dedb882a37b5b7c
DLhead2000 <abhishek.singh@giksindia.com>
2023-07-31 13:33:06 +0530

    add new my account page

--- src/Components/UserProfile/userprofile.js
+++ src/Components/UserProfile/userprofile.js
@@ -4,37 +4,36 @@ import "./userprofile.css"
 const userprofile= ()=>{
 return(
     <>
-<section className="myAccount-area">
-            <div className="Myacount-page">
-                <form>
-                    <div className="container mt-5 mb-4">
-                        <div className="row">
-                            <div className="col-12 Myacount-page">
-                                <div className="card p-4 m-2">
-                                    <h4 className="Myacount-heading text-center pb-3">My Account</h4>
-                                    <div className="row">
-                                        <div className="col-md-6 col-12 p-1">
-                                                <p className="ps-1">Name:</p>
-                                                <input type="text" className="form-control" placeholder row="3" name="name"></input>
-                                        </div>
-                                        <div className="col-md-6 col-12 p-1">
-                                                <p className="ps-1">Mobile Number:</p>
-                                                <input type="number" className="form-control bg-light" placeholder row="3" name="mobileNumber"></input>
-                                        </div>
-                                        <div className="col-md-6 col-12 p-1">
-                                                <p className="ps-1">Email:</p>
-                                                <input type="email" className="form-control bg-light" placeholder row="3" name="email"></input>
-                                        </div>
-                                        <div className="col-12 d-flex justify-content-center mt-4 p-2">
-                                            <button className="Myacount-button btn btn-primary" type="submit">Submit</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
+<section>
+<div className="Myacount-page">
+    <form>
+    <div className="container">
+    <div className="col-12 acount-page">
+    <div class="card">
+  <span class="title">My Account</span>
+  <form class="form">
+    <div class="group">
+    <input placeholder="" type="text" required=""></input>
+    <label for="name">Name</label>
+    </div>
+<div class="group">
+    <input placeholder="" type="text" name="address" required=""></input>
+    <label for="address">Address</label>
+    </div>
+<div class="group">
+    <input placeholder="" type="number" id="address" name="number" required=""></input>
+    <label for="number">Mobile Number</label>
+</div>
+<div className="account-button">
+    <button type="submit">Submit</button>
+    </div>
+  </form>
+</div>
+
+    </div>
+    </div>
+    </form>
+</div>
 </section>
     </>
 )
